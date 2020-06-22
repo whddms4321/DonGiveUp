b9bdaf569cb22327e9f7dbdad0c0f4cab5c4deff
baejaehyeon <qowo0420@naver.com>
2020-06-22 17:09:24 +0900

    boardrequestList.jsp 정렬까지 추가

--- donGiveUp/src/main/java/kr/co/don/adminMypage/controller/AdminMypageController.java
+++ donGiveUp/src/main/java/kr/co/don/adminMypage/controller/AdminMypageController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminMypageController {
 	
+	//관리자 마이페이지 메인(DashBoard) 이동
 	@RequestMapping(value="/dashboard.do")
 	public String dashboard() {
 		return "/mypage/admin/dashboard";
+	}
+	
+	//글 등록 신청목록 이동
+	@RequestMapping(value="/boardRequestList.do")
+	public String boardRequestList() {
+		return "/mypage/admin/boardRequestList";
 	}
 }
