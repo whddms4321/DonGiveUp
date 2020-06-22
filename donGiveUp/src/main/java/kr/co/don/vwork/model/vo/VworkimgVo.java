b7b1c82a4ad6912ee88966a517b10ea29d4e6ec3
DAHYUN-LEE-92 <65150088+DAHYUN-LEE-92@users.noreply.github.com>
2020-06-22 14:56:31 +0900

    ggg

---
+++ donGiveUp/src/main/java/kr/co/don/vwork/model/vo/VworkimgVo.java
@@ -0,0 +1,11 @@
+package kr.co.don.vwork.model.vo;
+
+import lombok.Data;
+
+@Data
+public class VworkimgVo {
+	private int vworkimgNo;
+	private String vworkimgFilename;
+	private String vworkimgFilepath;
+	private int vworkimgRef;
+}
